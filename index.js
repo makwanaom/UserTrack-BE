e80d0452305d3b82cd73d5c140141ce452a297e7
makwanaom <117265350+makwanaom@users.noreply.github.com>
2024-07-15 10:01:15 +0530

    sec commit

--- index.js
+++ index.js
@@ -1,52 +1,48 @@
 const express = require("express");
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 const UserModel = require('./models/users');
 require("./db/conn");
 
-const cors =  require('cors');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
-
 app.get("/getusers", async (req, res) => {
-    try {
-        const data = await UserModel.find();
-        res.send(data);
-      } catch (err) {
-        res.status(500).send(err.message);
-      }
-} )
-
-app.post("/createuser" ,  async (req, res) => {
-  const user = req.body;
-  const newUser = new UserModel(user);
-  await newUser.save();
-
-  res.json(user);
-
+  try {
+    const data = await UserModel.find();
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
-app.delete("/deleteuser/:username" , async (req, res) => {
+app.post("/createuser", async (req, res) => {
+  try {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.delete("/deleteuser/:username", async (req, res) => {
   try {
     const username = req.params.username;
     const user = await UserModel.findOneAndDelete({ username: username });
-
     if (!user) {
       return res.status(404).send("User not found");
     }
-
     res.json(user);
-
-  }
-  catch (err){
+  } catch (err) {
     res.status(500).send(err.message);
   }
-})
+});
 
-
-app.listen(8000,() => {
-    console.log("Server runs perfectly");
-})
-
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server runs perfectly on port ${PORT}`);
+});
