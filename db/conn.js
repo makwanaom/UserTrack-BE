b1f2026bfb6624df7f8b90d3feb96703fd1fda20
makwanaom <117265350+makwanaom@users.noreply.github.com>
2024-07-15 10:21:23 +0530

    third commit

--- db/conn.js
+++ db/conn.js
@@ -4,10 +4,16 @@ require('dotenv').config()
 
 const DB = process.env.URI;
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  };
 
 
-mongoose.connect(DB, {
 
-}).then(() => console.log("DataBase Connected")).catch((errr) => {
+mongoose.connect( DB, options)
+.then(() => console.log("DataBase Connected")).catch((errr) => {
     console.log(errr);
 })
